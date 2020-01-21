be1f26aa2726d5249ba79a24f9ac7041bb702023
Voltenko <darthvolt@gmail.com>
2020-01-21 12:10:25 +0300

    Base structure has been created

---
+++ src/components/control/Control.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+    
+</template>
+
+<script>
+    import ClosedControl from "./components/ClosedControl";
+    import OpenedControl from "./components/OpenedControl";
+
+    export default {
+        name: "Control",
+
+        components: {ClosedControl, OpenedControl},
+    }
+</script>
+
+<style lang="sass" scoped>
+
+</style>
---
+++ src/components/control/components/OpenedControl.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+    
+</template>
+
+<script>
+    export default {
+        name: "OpenedControl"
+    }
+</script>
+
+<style lang="sass" scoped>
+
+</style>
